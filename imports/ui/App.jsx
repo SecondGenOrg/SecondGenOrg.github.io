fd179e666369fc7f511c69ce4d8fedb3eda717c3
Avik Jain <avikjain12345@gmail.com>
2016-07-20 17:34:51 -0700

    imported material ui

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
- 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
 // App component - represents the whole app
 export default class App extends Component {
  
   render() {
     return (
-      <div>
-        Hello World
-      </div>
+    	<MuiThemeProvider>
+      	<RaisedButton label="Default" />
+      </MuiThemeProvider>
     );
   }
 }
