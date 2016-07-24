de1386422615be0579e54ec0e9e38117042d0f86
Avik Jain <avikjain12345@gmail.com>
2016-07-23 23:10:09 -0700

    Move subscribe form to banner and add partners

--- client/imports/modules/public/components/Home.jsx
+++ client/imports/modules/public/components/Home.jsx
@@ -9,21 +9,10 @@ export default class Home extends React.Component {
         return (
         		<div id="home-wrapper" className="content-wrapper">
         			<section id="section-splash">
-        				<div className="row">
-        					<div id="subscribe-panel" className="panel col-md-3 col-md-offset-1">
-        						<h2 style={subscribeTextStyle}>Subscribe for updates.</h2>
-        						<TextField 
-        							hintText="you@example.com"
-        						/>
-        						<FlatButton
-        							primary={true}
-        							label="Subscribe"
-        						/>
-        					</div>
-        				</div>
+
             	</section>
             	<section id="section-what-we-do">
-            		<h1 id="what-we-do-header" className="center-align">What We Do</h1>
+            		<h1 id="what-we-do-header" className="section-header">What We Do</h1>
             		<div className="row">
             			<div className="col-md-3 what-we-do-panel">
             				<img src="https://support.files.wordpress.com/2009/07/pigeony.jpg?w=688"/>
@@ -44,6 +33,42 @@ export default class Home extends React.Component {
             				<img src="https://support.files.wordpress.com/2009/07/pigeony.jpg?w=688"/>
             				<h3>Resources</h3>
             				<p>Bacon ipsum dolor amet beef ribs jowl pig short ribs drumstick boudin. Rump pancetta sirloin flank meatloaf spare ribs, shankle leberkas sausage andouille.</p>
+            			</div>
+            		</div>
+            	</section>
+            	<div id="subscribe-banner">
+            		<div className="container">
+            			<div className="row">
+		    						<TextField 
+		    							id="email-field"
+		    							fullWidth={true}
+		    							inputStyle={{color: 'white'}}
+		    							underlineFocusStyle={{borderColor: 'white'}}
+		    							hintText="you@example.com"
+		    							className="col-md-8"
+		    						/>
+		    						<FlatButton
+		    							label="Subscribe for Updates"
+		    							className="col-md-4"
+		    							primary={true}
+		    							backgroundColor="#ffffff"
+		    							hoverColor="#dddddd"
+		    						/>
+	    						</div>
+    						</div>
+    					</div>
+            	<section id="section-partners">
+            		<h1 id="partners-header" className="section-header">Partners</h1>
+            		<div className="row">
+            			<div className="col-md-4 col-xs-12 img-wrapper">
+            				<img src="https://lh6.googleusercontent.com/wA4GFlCner9YTlZIKBigAAYBDHUodJa93F3nzPuo1FJCjt1s7Oo1-gMyj_qefpUjsPGwfw"/>
+            			</div>
+            			<div className="col-md-4 col-xs-12 img-wrapper">
+            				<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Hewlett_Packard_Enterprise_logo.svg/1000px-Hewlett_Packard_Enterprise_logo.svg.png"/>
+            			</div>
+            			<div className="col-md-4 col-xs-12 img-wrapper">
+            				// find image with blue text
+            				<img src="https://cdn.makeschool.com/assets/main/logo-40c556abff7b50c3d7d09d6a7323ec05.svg"/>
             			</div>
             		</div>
             	</section>
--- client/imports/theme.js
+++ client/imports/theme.js
@@ -6,20 +6,20 @@ import zIndex from 'material-ui/styles/zIndex';
 
 // I grabbed these from a different project. Colors need to be changed
 export default {
-    spacing: Spacing/*,
+    spacing: Spacing,
     palette: {
-        primary1Color: '#2196F3', // Gold
+        primary1Color: '#2196f3', // Gold
         primary2Color: '#00af5b', // Green
         primary3Color: '#ee532c', // Red
         accent1Color: '#00af5b', // Green
         accent2Color: '#ee532c', // Red
         accent3Color: Colors.pinkA100,
-        textColor: '#f5f0e5',
+        textColor: '#000000',
         alternateTextColor: '#201e1c',
         canvasColor: '#201e1c',
         borderColor: ColorManipulator.fade('#f5f0e5', 0.3),
         disabledColor: ColorManipulator.fade('#f5f0e5', 0.3),
         pickerHeaderColor: 'white',
         zIndex,
-    }*/
+    }
 };
