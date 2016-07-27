9e5f4365be6d42fcd09931ae90740890b7763f8c
Avik Jain <avikjain12345@gmail.com>
2016-07-27 09:10:08 -0700

    Add empty chapters page

---
+++ client/imports/modules/public/components/Chapters.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Chapters extends React.Component {
+    render() {
+        return (
+            <h1>Chapters</h1>
+        );
+    }
+}
--- client/imports/modules/public/index.js
+++ client/imports/modules/public/index.js
@@ -1,16 +1,23 @@
 import Home from './components/Home.jsx';
-import Events from './components/Events.jsx'
+import Events from './components/Events.jsx';
+import Chapters from './components/Chapters.jsx';
 export { Home as Home };
 export { Events as Events };
+export { Chapters as Chapters }
 
 export function initialize() {
 
 }
 
-export const routes = [{
+export const routes = [
+	{
     path: 'home',
     component: Home,
-}, {
-	path: 'events',
-	component: Events
-}];
+	}, {
+		path: 'events',
+		component: Events
+	}, {
+		path: 'chapters',
+		component: Chapters
+	}
+];
