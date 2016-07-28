f95ea23baac7e7e133eb34637c715156b64d24d3
Avik Jain <avikjain12345@gmail.com>
2016-07-28 15:28:49 -0700

    Make events styles apply to other pages

--- client/imports/modules/public/components/Chapters.jsx
+++ client/imports/modules/public/components/Chapters.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import ChaptersMap from './ChaptersMap'
+import ChaptersMap from './ChaptersMap';
+import Paper from 'material-ui/Paper';
 export default class Chapters extends React.Component {
     render() {
         return (
             <div id="chapters-wrapper" className="page-wrapper">
-            		<section className="header-wrapper valign-wrapper"> 
+            	<section className="header-wrapper valign-wrapper"> 
                     <div className="header-text valign">
                         <h1>Chapters</h1>
                         <p>Some text about chapters or something blah blah blah.</p>
                     </div>
-			            	<ChaptersMap
-			            		id="chapters-map"
-			            	/>
+		            	<ChaptersMap
+		            		id="chapters-map"
+		            	/>
                 </section>
-                <a style={{marginTop: '60vh'}} href="/">asdf</a>
+                <div className="content-wrapper">
+                    <Paper id="cover" />
+                </div>
             </div>
 
         );
