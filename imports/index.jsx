78e9fe096175ef9b42e9b5c4cd8e311dc12d2c39
Avik Jain <avikjain12345@gmail.com>
2016-08-06 22:30:48 -0700

    Add Google Analytics

--- imports/index.jsx
+++ imports/index.jsx
@@ -4,18 +4,19 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import loadModules from './loadModules';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-
 import * as Public from './modules/public';
-
+import ReactGA from 'react-ga';
 injectTapEventPlugin();
 const root = loadModules(Public.Home, Public);
-console.log('root: ', root);
 if(typeof document !== 'undefined') {
+    ReactGA.initialize('UA-82081367-1');
     const router = (
     	<Router
     		onUpdate={
     			() => {
-        			window.scrollTo(0, 0)
+        			window.scrollTo(0, 0);
+                    ReactGA.set({ page: window.location.pathname });
+                    ReactGA.pageview(window.location.pathname);
         		}
     		} 
     		history={browserHistory} routes={root} 
