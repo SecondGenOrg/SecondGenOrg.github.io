0ee05dd46786fecc03006d317fc528aebf989288
Avik Jain <avikjain12345@gmail.com>
2016-10-11 08:56:38 -0700

    Update Contests page

--- imports/modules/public/components/Contests.jsx
+++ imports/modules/public/components/Contests.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Paper from 'material-ui/Paper';
+import Dialog from 'material-ui/Dialog';
 import * as Colors from 'material-ui/styles/colors';
+import { Card, CardActions, CardHeader, CardText, CardMedia, CardTitle } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 
 export default class Contests extends React.Component {
 		constructor() {
@@ -12,8 +15,46 @@ export default class Contests extends React.Component {
       };
       const errorMessage = false;
       const buttonDisabled = false;
-      this.state = { styles, errorMessage, buttonDisabled };
+      var contests = [
+        {
+          name: 'Business Plan'
+        },
+        {
+          name: 'Photography'
+        },
+        {
+          name: 'Web & Mobile Development'
+        },
+        {
+          name: 'Hardware Product'
+        },
+        {
+          name: 'Short Film'
+        },
+        {
+          name: 'Digital Art'
+        },
+        {
+          name: 'Traditional Art'
+        },
+        {
+          name: 'Startup'
+        },
+        {
+          name: 'Club Event'
+        },
+        {
+          name: 'Distinguished Club'
+        }
+      ]
+      this.state = { styles, errorMessage, buttonDisabled, contests, open: false };
 		}
+    handleClose() {
+      this.setState({open: false})
+    }
+    handleOpen() {
+      this.setState({open: true})
+    }
     render() {
     		const { styles } = this.state;
         return (
@@ -32,8 +73,40 @@ export default class Contests extends React.Component {
                       <p>Each competition event is geared towards the main fields that SecondGen members are involved in. They include business, photography, mobile development, hardware, film, digital art, traditional art, and startups. These events are guided by specific guidelines and informational sheets that can be found below. Each event has its own unique form of competing.</p>
                       <p>The competitive events are divided into individual, team, and chapter categories. Individual and team events focus on more on developed projects and learned skills. Chapter events on the other hand award SecondGen chapter that have contributed to one or more of these fields for community benefit.</p>
                   </Paper>
-                    <Paper id="cover" zDepth={5}/>
+                  <br/>
+                  <div className="row">
+                  {
+                    this.state.contests.map(function(contest) {
+                      return (
+                        <div className="col-xs-12 col-sm-4" key={contest.name} style={{paddingBottom: '3vh'}}>
+                          <Card zDepth={2}>
+                            <CardTitle 
+                              title={contest.name}
+                              titleStyle={{fontSize: '2.7vh', width: '100%'}}/>
+                            <CardActions>
+                              <FlatButton disabled={false} label="Guidelines" primary={true} onClick={this.handleOpen.bind(this)}/>
+                              <FlatButton disabled={true} label="Submit" />
+                            </CardActions>
+                          </Card>
+                        </div>
+                      )
+                    }, this)
+                  }
+                  </div>
+                  <Paper id="cover" zDepth={5}/>
                 </div>
+                <Dialog
+                  actions={[<FlatButton
+                    label="Cancel"
+                    primary={true}
+                    onTouchTap={this.handleClose.bind(this)}
+                  />]}
+                  modal={false}
+                  open={this.state.open}
+                  onRequestClose={this.handleClose.bind(this)}
+                >
+                  More information coming November 1st.
+                </Dialog>
             </div>
         );
     }
